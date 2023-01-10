7e56738500e8514afd7f2768f6ae07521647ad29
Luiz Fernanado <75041533+luizpibo@users.noreply.github.com>
2023-01-10 15:32:47 -0300

    ...

---
+++ ParametersAndExceptionHandler/src/main/java/br/com/ParametersAndExceptionHandler/ParametersAndExceptionHandler.java
@@ -0,0 +1,13 @@
+package br.com.ParametersAndExceptionHandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParametersAndExceptionHandler {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParametersAndExceptionHandler.class, args);
+	}
+
+}
---
+++ ParametersAndExceptionHandler/src/test/java/br/com/primeiros_passos/PrimeirosPassosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.primeiros_passos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeirosPassosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

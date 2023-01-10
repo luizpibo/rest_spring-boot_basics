aa5e952a16137bfecd1ccc62a9547a1edc896ec4
Luiz Fernanado <75041533+luizpibo@users.noreply.github.com>
2023-01-10 17:27:47 -0300

    adicionando service

--- DependencyInjection/src/main/java/br/com/DependencyInjection/controllers/AppController.java
+++ DependencyInjection/src/main/java/br/com/DependencyInjection/controllers/AppController.java
@@ -1,11 +1,19 @@
 package br.com.DependencyInjection.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.DependencyInjection.services.AppService;
+
 @RestController
-public class appController {
+public class AppController {
+	
+	@Autowired
+	public final AppService appService = new AppService();
+	
+	
 	@GetMapping("/")
 	public ResponseEntity<String> home() {
 		return ResponseEntity.ok("teste");
---
+++ DependencyInjection/src/main/java/br/com/DependencyInjection/services/AppService.java
@@ -0,0 +1,31 @@
+package br.com.DependencyInjection.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppService {
+
+	public static Double sum(Double num1, Double num2) {
+		return num1 + num2;
+	}
+
+	public static Double multiplication(Double num1, Double num2) {
+		return num1 * num2;
+	}
+
+	public static Double division(Double num1, Double num2) {
+		return num1 / num2;
+	}
+
+	public static Double mean(Double num1, Double num2) {
+		return (sum(num1, num2)) / 2;
+	}
+
+	public static Double pow(Double num1, Double num2) {
+		return Math.pow(num1, num2);
+	}
+
+	public static Double squareRoot(Double num) {
+		return Math.sqrt(num);
+	}
+}
